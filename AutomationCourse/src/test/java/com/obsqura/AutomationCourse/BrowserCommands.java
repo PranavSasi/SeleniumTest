26c9aeb8691d072ae92000999e07fa53ea4df2c1
KuAp <KuAp@LAPTOP-E8K2MQHG>
2023-08-22 01:10:23 +0530

    Browser Load and Close/Quit

---
+++ AutomationCourse/src/test/java/com/obsqura/AutomationCourse/BrowserCommands.java
@@ -0,0 +1,14 @@
+package com.obsqura.AutomationCourse;
+
+public class BrowserCommands extends Base{
+
+	public static void main(String[] args) {
+	
+		BrowserCommands browsercommands=new BrowserCommands();
+		browsercommands.initialiseBrowser();
+		browsercommands.driverCloseOrQuit();
+		
+		
+	}
+
+}
